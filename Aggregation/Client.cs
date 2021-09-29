7551d1474d9cf7657f841608ae063ced26d4045f
Yevhenii Klimov <24evgeniy03@gmail.com>
2021-09-29 18:32:35 +0000

    Update Client.cs

--- Aggregation/Client.cs
+++ Aggregation/Client.cs
@@ -1,16 +1,56 @@
 namespace Aggregation
 {
-    //TODO: Define public class "Client".
-    
-    //TODO: Define private field "deposits" with type of array of "Deposit".
-    
-    //TODO: Define parameterless constructor that creates empty array of deposits with size of 10.
-    
-    //TODO: Define public method "AddDeposit" that takes deposit and adds it to "deposits". If the array of deposits has empty place, returns true, else returns false.
-    
-    //TODO: Define public method "TotalIncome" that returns total income of all deposits upon their expiration. 
-    
-    //TODO: Define public method "MaxIncome" that returns max value of income of all deposits of client.
-    
-    //TODO: Define public method "GetIncomeByNumber" that takes number of deposit (index of deposit in array plus 1) that returns income of this deposit.
+    public class Client
+    {
+        private Deposit[] deposits;
+
+        public Client()
+        {
+            deposits = new Deposit[10];
+        }
+
+        public bool AddDeposit(Deposit deposit)
+        {
+            for (int i = 0; i < deposits.Length; i++)
+            {
+                if (deposits[i] == null)
+                {
+                    deposits[i] = deposit;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public decimal TotalIncome()
+        {
+            decimal result = 0;
+            foreach (Deposit deposit in deposits)
+            {
+                if(deposit != null)
+                    result += deposit.Income();
+            }
+            return result;
+        }
+
+        public decimal MaxIncome()
+        {
+            decimal max = decimal.MinValue;
+            for (int i = 0; i < deposits.Length; i++)
+            {
+                if (deposits[i] != null && deposits[i].Income() > max)
+                    max = deposits[i].Income();
+            }
+            return max;
+        }
+
+        public decimal GetIncomeByNumber(int number)
+        {
+            if (number <= deposits.Length && deposits[number - 1] != null)
+            {
+                return deposits[number - 1].Income();
+            }
+            else return 0;
+        }
+    }
 }
